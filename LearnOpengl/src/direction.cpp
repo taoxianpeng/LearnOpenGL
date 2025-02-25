3d298ff1acfb323427310d22137ee52b1fe04162
Xianpeng Tao <492350627@qq.com>
2025-02-25 23:21:10 +0800

    修改 适配xmake for linux

--- LearnOpengl/src/direction.cpp
+++ LearnOpengl/src/direction.cpp
@@ -1,21 +1,21 @@
+#include "direction.h"
+#include "log.h"
 #include <exception>
 #include <filesystem>
 #include <fstream>
-#include "direction.h"
-#include "log.h"
 
-Direction* Direction::m_direction = nullptr;
+Direction *Direction::m_direction = nullptr;
 
-Direction::Direction() {
-}
+Direction::Direction() {}
 
-void Direction::loadConfig(const std::filesystem::path& path) {
-    try {
-        std::ifstream f(path.string());   
-        config = njson::parse(f);
-        m_projectRootPath = std::filesystem::path(std::string(config["projection_root_path"]));
-        LOGD("projectRootPath: {}", m_projectRootPath);
-    } catch (std::exception& e) {
-        LOGE("{}", e.what());
-    }
+void Direction::loadConfig(const std::filesystem::path &path) {
+  try {
+    std::ifstream f(path.string());
+    config = njson::parse(f);
+    m_projectRootPath =
+        std::filesystem::path(std::string(config["projection_root_path"]));
+    LOGD("projectRootPath: {}", m_projectRootPath.string());
+  } catch (std::exception &e) {
+    LOGE("{}", e.what());
+  }
 }
